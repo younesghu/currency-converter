627a24b1a3a116c6584d4314447287916b7e284b
Younes Guigaho <younesghu44@gmail.com>
2023-10-11 21:48:49 +0100

    step back

--- app/Http/Controllers/CurrencyConverterController.php
+++ app/Http/Controllers/CurrencyConverterController.php
@@ -13,27 +13,4 @@ class CurrencyConverterController extends Controller
         return view('currency-converter');
     }
 
-    public function convert(Request $request)
-    {
-        $amount = $request->input('amount');
-        $fromCurrency = $request->input('from_currency');
-        $toCurrency = $request->input('to_currency');
-
-        $response = Http::get($this->apiUrl, [
-            'api_key' => config('910504e83ebb5fc1b4eef142d91013db'),
-            'from' => $fromCurrency,
-            'to' => $toCurrency,
-        ]);
-
-        $data = $response->json();
-
-        if ($data['success']) {
-            $conversionRate = $data['rates'][$toCurrency];
-            $result = $amount * $conversionRate;
-        } else {
-            $result = 'Error fetching data';
-        }
-
-        return view('currency-converter', compact('result'));
-    }
 }
