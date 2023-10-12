5ce72e81fc55bca0ed77f2b6e866c1fb32ec0c25
Younes Guigaho <younesghu44@gmail.com>
2023-10-12 12:01:03 +0100

    fixed external API

--- app/Http/Controllers/CurrencyConverterController.php
+++ app/Http/Controllers/CurrencyConverterController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CurrencyConverterController extends Controller
 {
-    protected $apiUrl = 'http://api.exchangeratesapi.io/v1/latest';
+ //   protected $apiUrl = 'http://api.exchangeratesapi.io/v1/latest';
 
     public function index()
     {
-        return view('currency-converter');
+        $response = Http::get('http://api.exchangeratesapi.io/v1/latest?access_key=910504e83ebb5fc1b4eef142d91013db');
+
+        $jsonData = $response->json();
+        dd($jsonData);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,3 @@ use App\Http\Controllers\CurrencyConverterController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', [CurrencyConverterController::class, 'index']);
