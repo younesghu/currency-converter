a6b93b724d027050e40f98e6d9d3144f329efbcb
Younes Guigaho <younesghu44@gmail.com>
2023-10-12 12:09:16 +0100

    fixed get function

--- app/Http/Controllers/CurrencyConverterController.php
+++ app/Http/Controllers/CurrencyConverterController.php
@@ -9,12 +9,12 @@ class CurrencyConverterController extends Controller
 {
  //   protected $apiUrl = 'http://api.exchangeratesapi.io/v1/latest';
 
-    public function index()
+    public function getall()
     {
         $response = Http::get('http://api.exchangeratesapi.io/v1/latest?access_key=910504e83ebb5fc1b4eef142d91013db');
 
         $jsonData = $response->json();
-        dd($jsonData);
+        return $jsonData;
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/currencies', [CurrencyConverterController::class, 'index']);
+Route::get('/currencies', [CurrencyConverterController::class, 'getall']);
 
